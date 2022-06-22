058b95778f10f6b812a9d3fc49fc51ad1c1b75da
Mefesta <99808406+Mefesta@users.noreply.github.com>
2022-06-22 22:02:02 +0300

    Add files via upload

---
+++ sem 2/ppe/vs/lab 2/WindowsProject2/WindowsProject2/WindowsProject2.cpp
@@ -0,0 +1,127 @@
+﻿#include <windows.h> //conține declarații pentru toate funcțiile din API-ul  
+//Windows, toate macrocomenzile comune utilizate in window
+#include <math.h> /// operate matematice de bază
+#define NUM	1000
+#define TWOPI	(2 * 3.14159)
+int a[3][6] = { { 123, 343, 100, 163, 232, 166 }, { 128, 15, 129, 74, 234, 255 }, { 1, 2,3,4,1,4} };
+int circle(HWND hwnd, HDC hdc, int i, int j, int dir, RECT rect, int nr) {
+	HPEN hYellowPen; HBRUSH hRedBrush;
+	//Pictează interiorul poligoanelor, elipselor și căilor.
+	hYellowPen = CreatePen(PS_SOLID, 1, RGB(255, 255, 0)); 
+	hRedBrush = CreateSolidBrush(RGB(255, 0, 0)); if (j + 150 > rect.bottom)
+		 
+	{
+		if (dir == 1)a[2][nr] = 2; else a[2][nr] = 3;
+	}
+	if (i + 150 > rect.right)
+	{
+		if (dir == 2)a[2][nr] = 3; else a[2][nr] = 4;
+	}
+	if (j + 50 < rect.top)
+	{
+		if (dir == 3)a[2][nr] = 4; else a[2][nr] = 1;
+	}
+	if (i + 50 < rect.left)
+	{
+		if (dir == 4)a[2][nr] = 1; else a[2][nr] = 2;
+	}
+	switch (a[2][nr])
+	{
+	case 1: {
+		a[0][nr]++; a[1][nr]++; break;
+	}
+	case 2: {
+		a[0][nr]++; a[1][nr]--; break;
+	}
+	case 3: {
+		a[0][nr]--; a[1][nr]--; break;
+	}
+	case 4: {
+		a[0][nr]--; a[1][nr]++; break;
+	} }
+	switch (nr)
+	{
+	case 0: {SelectObject(hdc, hYellowPen); SelectObject(hdc, hRedBrush); break; }
+	}
+	Rectangle(hdc, a[0][nr] + 50, a[1][nr] + 50, a[0][nr] + 150, a[1][nr] + 150);
+	DeleteObject(hYellowPen); DeleteObject(hRedBrush);
+	// stergerea obiectelor create
+	return dir;
+}
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+// este un indicator de funcție care este apelat de fiecare dată când este îndeplinită oanumită condiție
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+// este convenția de apelare . O convenție de apelare definește modul în care o funcție primește parametrii de la apelant.
+{
+	static char szAppName[] = "SineWave"; // setează numele
+	HWND	hwnd; // parametru manerul ferestrei este null
+	MSG	msg;
+	WNDCLASSEX	wndclass;
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL; wndclass.lpszClassName = (LPCWSTR)szAppName; wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION); RegisterClassEx(&wndclass);
+	hwnd = CreateWindow((LPCWSTR)szAppName, L"Interfata GDI/Bunescu Gabriel Gr.TI-207",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL); ShowWindow(hwnd, iCmdShow); UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); DispatchMessage(&msg);//  face unele procesări suplimentare pe tastatură
+	}
+	return msg.wParam;//bucla de mesaj 
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+//este un indicator de funcție care este apelat de fiecare dată când este îndeplinită o anumită condiție
+{
+	static int	cxClient, cyClient;
+	HDC	hdc, hCompatibleDC; //Un mâner pentru afișaj DC pentru a fi folosit pentru vopsire.
+	int i; PAINTSTRUCT ps; //Structura PAINTSTRUCT conține informații pentru o aplicație.
+	//Aceste informații pot fi folosite pentru a picta zona client a unei ferestre deținute de acea aplicație.
+	POINT	pt[NUM];
+	PAINTSTRUCT PaintStruct;
+	HANDLE hBitmap, hOldBitmap;
+	RECT Rect;
+	BITMAP Bitmap;
+	switch (iMsg)
+	{
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam); return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps); // Desenam linii dreapte
+		hBitmap = LoadImage(NULL, L"asd.BMP", //gestionați un bitmap GDI. 
+			IMAGE_BITMAP, 700, 430, LR_LOADFROMFILE);/// Obtinem marimea imaginii bitmap
+		GetObject(hBitmap, sizeof(BITMAP), &Bitmap); hCompatibleDC = CreateCompatibleDC(hdc); hOldBitmap = SelectObject(hCompatibleDC, hBitmap); GetClientRect(hwnd, &Rect);
+		//Cream o noua variabila handle a contextului de dispozitiv apoi Pastram imaginea veche bitmap, 
+		//Copiem imaginea bitmap si o deplasam
+		StretchBlt(hdc, 0, 0, cxClient, cyClient, hCompatibleDC, 0, 0, Bitmap.bmWidth, Bitmap.bmHeight, SRCCOPY);
+		// Restauram obiectul anterior
+		SelectObject(hCompatibleDC, hOldBitmap);
+		//Stergem imaginea bitmap incarcata
+		DeleteObject(hBitmap);
+		//Stergem variabila handle a contextului de dispozitiv
+		DeleteDC(hCompatibleDC);
+		DrawText(hdc, L"Lucrare de laborator Nr2 ", -1, &Rect,
+			DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		for (i = 0; i < NUM; i++) {
+			pt[i].x = i * cxClient / NUM;
+			pt[i].y = (int)(cyClient / 2 *
+				(1 - sin(TWOPI * i / NUM)));
+		}
+		Polyline(hdc, pt, NUM);
+		InvalidateRect(hwnd, NULL, TRUE);
+		circle(hwnd, hdc, a[0][0], a[1][0], a[2][0], Rect, 0); Sleep(50);
+		EndPaint(hwnd, &PaintStruct); return 0;
+	case WM_DESTROY: PostQuitMessage(0); return 0;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
---
+++ sem 2/ppe/vs/lab 3/WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,173 @@
+﻿#include <windows.h> 
+//conține declarații pentru toate funcțiile din API-ul Windows, toate macrocomenzile comune utilizate in windows
+#include <tchar.h> //operate matematice de bază
+#define IDM_EXIT 1
+#define IDM_CLEAR 4
+#define IDM_BEZ1 100
+bool bezier1 = false;
+HINSTANCE hInst;
+POINT Pt[4] = { { 100, 48 }, { 88, 246 }, { 364, 192 }, { 250, 48 } };
+int varvar = 0;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);//este un indicator de funcție care este apelat de 
+//fiecare dată când este îndeplinită o anumită condiție
+void mybezier(HDC hdc, const POINT* Pt) {
+	mybezier(hdc, Pt);
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR szCmdLine, int iCmdShow)
+	// este convenția de apelare . O convenție de apelare definește modul în 
+	//care o funcție primește parametrii de la apelant.
+{
+	static char szAppName[] = "HelloWin"; // setează numele
+	HWND hwndMain; // parametru manerul ferestrei este null
+	MSG msg;
+	WNDCLASSEX wndclass;
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	// Indicator la procedura ferestrei pentru această clasă de ferestre
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;// Handle la instanța aplicației (pe care am 
+	//primit - o în primul parametru al WinMain()
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);// Pictogramă 
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);//culoarea fundalului 
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = L"MyWindowClass";// numele care identifica clasa 
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&wndclass);
+	hwndMain = CreateWindowEx(
+			WS_EX_CLIENTEDGE, // este stilul ferestre extinse,
+			L"MyWindowClass",
+			L"Lab3",
+			WS_OVERLAPPEDWINDOW, // stilul ferestrei
+			CW_USEDEFAULT, CW_USEDEFAULT, 800, 500,
+			NULL, NULL, hInstance, NULL);  // mânerul instanței aplicației și un 
+	//pointer către datele de creare a ferestrei.
+	ShowWindow(hwndMain, iCmdShow);
+	UpdateWindow(hwndMain);
+	while (GetMessage(&msg, NULL, 0, 0))
+		// GetMessage()primește un mesaj din coada de mesaje a aplicației 
+	{
+		TranslateMessage(&msg); // face unele procesări suplimentare pe tastatură
+		DispatchMessage(&msg); ; // trimite mesajul către fereastră că mesajul a fost trimis 
+	}
+	return msg.wParam; //bucla de mesaj
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+// este un indicator de funcție care este apelat de fiecare dată când este îndeplinită o anumită condiție
+{
+	int wmId, wmEvent;
+	HDC hdc;
+	PAINTSTRUCT ps;
+	//Structura PAINTSTRUCT conține informații pentru o  aplicație.Aceste informații pot 
+	//fi folosite pentru a picta zona client a unei ferestre deținute de acea aplicație.
+	RECT rect;  //Rect reprezintă un dreptunghi abstract și poate fi folosit într-o  varietate de situații
+	HMENU hMenubar = CreateMenu();
+	HMENU hFile = CreateMenu();
+	HMENU hOptions = CreateMenu();
+	switch (iMsg)
+	{
+	case WM_CREATE: {
+		// este un mesaj trimis atunci când o aplicație solicită
+		//crearea unei ferestre prin apelarea funcției CreateWindowEx
+		HMENU hMenubar = CreateMenu();
+		HMENU hFile = CreateMenu();
+		HMENU hOptions = CreateMenu();
+		AppendMenu(hMenubar, MF_POPUP, (UINT_PTR)hFile, L"File");
+		AppendMenu(hMenubar, MF_POPUP, NULL, L"Edit");
+		AppendMenu(hMenubar, MF_POPUP, (UINT_PTR)hOptions, L"Bezier");
+		AppendMenu(hFile, MF_STRING, IDM_EXIT, L"Exit");
+		AppendMenu(hOptions, MF_STRING, IDM_BEZ1, L"BEZ_1");
+		AppendMenu(hOptions, MF_STRING, IDM_CLEAR, L"CLEAR");
+		SetMenu(hwnd, hMenubar);
+		break;
+	}
+	case WM_COMMAND:
+		wmId = LOWORD(wParam); // lățimea spațiului de lucru
+		wmEvent = HIWORD(wParam);  // inaltimea spațiului de lucru
+		// Analizați selecția meniului: 
+		switch (LOWORD(wParam))
+		{
+		case IDM_EXIT:
+			DestroyWindow(hwnd);
+			break;
+		case IDM_BEZ1:
+			bezier1 = true;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		
+		case IDM_CLEAR:
+			bezier1 = false;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		default:
+			return DefWindowProc(hwnd, iMsg, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+	// este trimis atunci când sistemul sau o altă aplicație 
+		//solicită să picteze o porțiune din fereastra unei aplicații.
+		hdc = BeginPaint(hwnd, &ps);
+		if (bezier1)
+			PolyBezier(hdc, Pt, 4); //functia de desenare a polybenzier
+		
+		return 0;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_NUMPAD8:
+			Pt[3].y--;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_NUMPAD2:
+			Pt[3].y++;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_NUMPAD6:
+			Pt[3].x++;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_NUMPAD4:
+			Pt[3].x--;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_UP:
+			Pt[0].y--;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_DOWN:
+			Pt[0].y++;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_RIGHT:
+			Pt[0].x++;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case VK_LEFT:
+			Pt[0].x--;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_MOUSEMOVE:
+		if (GetKeyState(MK_LBUTTON))
+		{
+			Pt[varvar].x = LOWORD(lParam);
+			Pt[varvar].y = HIWORD(lParam);
+			InvalidateRect(hwnd, NULL, TRUE);
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		if (varvar == 3) varvar = -1;
+		varvar++;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
